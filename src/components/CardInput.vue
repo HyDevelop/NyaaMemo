9a9c5a382cc02443cc7f8a2164f01950a8791b71
Hykilpikonna <me@hydev.org>
2021-03-08 14:42:34 -0500

    [+] Create card view

---
+++ src/components/CardInput.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="word-input">
+        <div id="title">{{ title }}</div>
+        <el-button id="btn-reveal-ans">Reveal Answer</el-button>
+        <el-input v-model="input"></el-input>
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({components: {}})
+export default class CardInput extends Vue
+{
+    input = "Long time no see, it has been a while"
+    title = "おひさしぶり"
+}
+</script>
+
+<style lang="scss" scoped>
+#word-input
+{
+    max-width: 400px;
+    margin: auto;
+
+    #title
+    {
+        font-size: x-large;
+        margin-bottom: 50px;
+    }
+
+    #btn-reveal-ans
+    {
+        margin-bottom: 50px;
+    }
+}
+</style>
