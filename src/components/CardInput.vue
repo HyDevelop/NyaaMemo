3601406a37a6435f9e4c7c12c93bf19aac1b561a
Hykilpikonna <me@hydev.org>
2021-03-08 19:28:52 -0500

    [+] Display second word form

--- src/components/CardInput.vue
+++ src/components/CardInput.vue
@@ -1,18 +1,22 @@
 <template>
     <div id="word-input">
-        <div id="title">{{ 'test' }}</div>
+        <div id="title">
+            <div id="word">{{ card.word[0] }}</div>
+            <div id="wordForm2">{{ card.word[1] }}</div>
+        </div>
+        <div id="subtitle"></div>
         <el-button id="btn-reveal-ans">Reveal Answer</el-button>
-        <el-input v-model="input"></el-input>
+        <el-input id="input" v-model="input"></el-input>
     </div>
 </template>
 
 <script lang="ts">
-import {Options, Vue} from 'vue-class-component';
+import {Options, prop, Vue} from 'vue-class-component';
 import {Card} from "@/logic/models";
 
 class Props
 {
-    card!: Card
+    card = prop<Card>({required: true})
 }
 
 @Options({components: {}})
@@ -25,13 +29,27 @@ export default class CardInput extends Vue.with(Props)
 <style lang="scss" scoped>
 #word-input
 {
-    max-width: 400px;
-    margin: auto;
-
     #title
     {
-        font-size: x-large;
         margin-bottom: 50px;
+        background-color: #e2eeff;
+
+        #word
+        {
+            font-size: xx-large;
+        }
+
+        #wordForm2
+        {
+            color: gray;
+            font-size: large;
+        }
+    }
+
+    #input
+    {
+        max-width: 400px;
+        margin: auto;
     }
 
     #btn-reveal-ans
--- src/views/ReviewScreen.vue
+++ src/views/ReviewScreen.vue
@@ -4,18 +4,25 @@
 
         </div>
 
-        <CardInput></CardInput>
+        <CardInput :card="card"></CardInput>
     </div>
 </template>
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
 import CardInput from "@/components/CardInput.vue";
+import {Card} from "@/logic/models";
 
 @Options({components: {CardInput}})
 export default class ReviewScreen extends Vue
 {
-
+    card: Card = {
+        word: ["猫", "ねこ"],
+        definition: ["n. Cat", "n. God :o"],
+        sentences: [
+            {sentence: "私わ猫です", translation: "I'm a cat"}
+        ]
+    }
 }
 </script>
 
