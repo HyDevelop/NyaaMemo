4b1f6d9d4ba3a4016f6fc55bf2f0ae280298cee0
Hykilpikonna <me@hydev.org>
2021-03-09 16:36:18 -0500

    [-] Remove about page

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,23 +5,16 @@ import Debug from "@/views/Debug.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
-        path: '/review',
-        name: 'Review',
-        component: ReviewScreen
-    },
-    {
         path: '/',
         name: 'Home',
         component: Home
     },
     {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        path: '/review',
+        name: 'Review',
+        component: ReviewScreen
     },
+    // Hidden
     {
         path: '/debaggu',
         name: 'デバッグ',
