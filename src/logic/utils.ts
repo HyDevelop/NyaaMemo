12bf6ab62b918b99c375a112bb9d5c415aa02dbd
Hykilpikonna <me@hydev.org>
2021-03-08 21:09:51 -0500

    [+] Create util function to get n random items in an arrayq

---
+++ src/logic/utils.ts
@@ -0,0 +1,10 @@
+
+// https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
+export function rand<T>(array: T[], n: number)
+{
+    // Shuffle array
+    const shuffled = array.sort(() => 0.5 - Math.random());
+
+    // Get sub-array of first n elements after shuffled
+    return shuffled.slice(0, Math.min(n, shuffled.length));
+}
