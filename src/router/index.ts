a877de2e02a8a1ff3da1ba8d1373e1873724317d
Hykilpikonna <me@hydev.org>
2021-03-09 16:32:51 -0500

    [+] Create debug view to test conversion

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 import Home from '../views/Home.vue'
 import ReviewScreen from "@/views/ReviewScreen.vue";
+import Debug from "@/views/Debug.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -20,7 +21,12 @@ const routes: Array<RouteRecordRaw> = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    },
+    {
+        path: '/debaggu',
+        name: 'デバッグ',
+        component: Debug
+    },
 ]
 
 const router = createRouter({
---
+++ src/views/Debug.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="Debug">
+        <el-input v-model="debugText" @input="onInputText"></el-input>
+        <div v-html="debugOutput"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import {okuriganaToFurigana} from "@/logic/utils";
+
+@Options({components: {}})
+export default class Debug extends Vue
+{
+    debugText = "感(かん)じ取(と)れたら手(て)を繋(つな)ごう、重(かさ)なるのは人生(じんせい)のライン and レミリア最高(さいこう)！"
+    debugOutput = ""
+
+    onInputText()
+    {
+        this.debugOutput = okuriganaToFurigana(this.debugText)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
