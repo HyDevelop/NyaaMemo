23780f770407b323824aac256f1c8282990f5b10
Hykilpikonna <me@hydev.org>
2021-03-10 18:22:52 -0500

    [+] Create my own input component

---
+++ src/components/HyInput.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="hy-input">
+        <label>
+            <input class="hy-input-inner" :modelValue="modelValue"
+                   @input="passInput($event.target.value)"/>
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HyInput",
+    props: {
+        modelValue: String
+    },
+    emits: [
+        'update:modelValue',
+        'input'
+    ],
+    methods: {
+        passInput(value) {
+            this.$emit('input', value)
+            this.$emit('update:modelValue', value)
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+input
+    -webkit-appearance: none
+    background-color: white
+    background-image: none
+    border-radius: 4px
+    border: 1px solid #DCDFE6
+    -webkit-box-sizing: border-box
+    box-sizing: border-box
+    color: #606266
+    display: inline-block
+    height: 40px
+    line-height: 40px
+    outline: 0
+    padding: 0 15px
+    width: 100%
+</style>
