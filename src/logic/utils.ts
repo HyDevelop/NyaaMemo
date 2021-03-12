ce2d63af81d1685613386182815c678fbeea4a48
Hykilpikonna <me@hydev.org>
2021-03-12 15:01:57 -0500

    [+] Add string similarity functions from SO

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -84,3 +84,60 @@ export function okuriganaToFurigana(okurigana: string)
 
     return result + kanjiCache
 }
+
+/**
+ * Find the number of changes required for one string to become another string
+ * https://stackoverflow.com/a/36566052/7346633
+ */
+function editDistance(s1: string, s2: string)
+{
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    const costs: number[] = [];
+
+    for (let i = 0; i <= s1.length; i++)
+    {
+        let lastValue = i;
+        for (let j = 0; j <= s2.length; j++)
+        {
+            if (i == 0) costs[j] = j;
+            else
+            {
+                if (j > 0)
+                {
+                    let newValue = costs[j - 1];
+                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                    {
+                        newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                    }
+                    costs[j - 1] = lastValue;
+                    lastValue = newValue;
+                }
+            }
+        }
+        if (i > 0) costs[s2.length] = lastValue;
+    }
+
+    return costs[s2.length];
+}
+
+/**
+ * Find the similarity proportion between two strings
+ * https://stackoverflow.com/a/36566052/7346633
+ */
+function similarity(s1: string, s2: string)
+{
+    let longer = s1;
+    let shorter = s2;
+
+    if (s1.length < s2.length)
+    {
+        longer = s2;
+        shorter = s1;
+    }
+
+    const longerLength = longer.length;
+    if (longerLength == 0) return 1.0;
+    return (longerLength - editDistance(longer, shorter)) / longerLength;
+}
