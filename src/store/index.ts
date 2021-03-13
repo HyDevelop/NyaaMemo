777d4337e2f1a97d57731110d661c7cf094fc397
Hykilpikonna <me@hydev.org>
2021-03-13 16:40:49 -0500

    [+] Create function to check if a word exist in localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,18 @@ import {createStore} from 'vuex'
 import VuexPersistence from "vuex-persist";
 import {LocalData} from "@/logic/models";
 
+/**
+ * Check if a word is already added to the long-term storage list.
+ */
+export function hasWord(state: LocalData, w: string): boolean
+{
+    w = w.toLowerCase()
+    return state.longTermWords.some(it => it.word.toLowerCase() == w)
+}
+
+/**
+ * Vuex automated persistence using localStorage
+ */
 const vuexLocal = new VuexPersistence<LocalData>({
     storage: window.localStorage
 })
