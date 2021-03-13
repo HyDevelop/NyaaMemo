7938a8fe7b65b98ad90ad4fa279bbcb8ce0c45d2
Hykilpikonna <me@hydev.org>
2021-03-13 09:35:56 -0500

    [+] Create console logging util functions

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -157,3 +157,25 @@ export function highlight(s: string, sub: string)
 {
     return s.replaceAll(sub, `<span class="color-highlight">${sub}</span>`)
 }
+
+
+// ///////////////////// //
+// Console Logging Utils //
+// ///////////////////// //
+
+const logPrefixCss = 'border-radius: 10px; padding: 1px 5px;'
+
+export function error(s: string)
+{
+    console.error(`%cERROR%c ${s}`, 'background: #ffe1e1; color: red;' + logPrefixCss, 'background: unset; color: red')
+}
+
+export function warning(s: string)
+{
+    console.log(`%cWARN%c ${s}`, 'background: #fdf6ec; color: #E6A23C;' + logPrefixCss, 'background: unset')
+}
+
+export function info(s: string)
+{
+    console.log(`%cINFO%c ${s}`, 'background: #f0f9eb; color: #67C23A;' + logPrefixCss, 'background: unset')
+}
