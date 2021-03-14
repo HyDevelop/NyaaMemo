f23b06b1990533f15422aa5c9ed84fd9e480e0af
Hykilpikonna <me@hydev.org>
2021-03-14 14:22:59 -0400

    [+] Create typing for this.$store

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
+// Create app
+const app = createApp(App)
+
+// Vuex
+import store from './store'
+import {Store} from "vuex";
+import {LocalData} from "@/logic/models";
+declare module '@vue/runtime-core' {
+    // provide typings for `this.$store`
+    interface ComponentCustomProperties {
+        $store: Store<LocalData>;
+    }
+}
+app.use(store)
+
+// Router
+import router from './router'
+app.use(router)
+
+// Element UI
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
+app.use(ElementPlus)
 
+// Cookies
 import VueCookies from 'vue3-cookies'
-
 // @ts-ignore
-createApp(App).use(store).use(router).use(ElementPlus).use(VueCookies).mount('#app')
+app.use(VueCookies)
+
+// Mount app
+app.mount('#app')
