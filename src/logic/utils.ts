6e887bc6825b386452b33f1572f54e32c6692836
Hykilpikonna <me@hydev.org>
2021-03-14 10:10:44 -0400

    [+] Import shuffle array function from SO

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -193,6 +193,30 @@ export function removeIf<T>(arr: T[], callback: (val: T, i: number) => boolean)
 }
 
 /**
+ * Shuffle array
+ * https://stackoverflow.com/a/2450976/7346633  
+ */
+function shuffle<T>(array: T[])
+{
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex)
+    {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/**
  * Highlight a string
  */
 export function highlight(s: string, sub: string)
