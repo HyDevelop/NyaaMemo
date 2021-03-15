05de21b0de0734adb71daa5cdbe1a485a9ca5614
Hykilpikonna <me@hydev.org>
2021-03-15 14:39:26 -0400

    [U] Update repo name in deploy script

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:HyDevelop/AnkiNG.git master:gh-pages
+git push -f git@github.com:HyDevelop/NyaaMemo.git master:gh-pages
 
 cd -
