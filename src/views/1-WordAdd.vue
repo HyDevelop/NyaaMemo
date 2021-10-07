a0a7e98fba406c12300887af4e391802d96c5ac9
Hykilpikonna <me@hydev.org>
2021-10-07 00:14:27 -0400

    [+] Create WordAdd vue

---
+++ src/views/1-WordAdd.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="word-add">
+        <div class="input-container">
+            <input />
+        </div>
+        <div class="input-container">
+            <input />
+        </div>
+    </div>
+</template>
+
+<script>
+import {Vue} from "vue-class-component";
+
+export default class WordAdd extends Vue
+{
+
+}
+</script>
+
+<style scoped>
+
+</style>
