accfa1544b37fce420121b8fde928b53b1445459
Hykilpikonna <me@hydev.org>
2021-10-07 00:18:18 -0400

    [+] WordAdd: Add page description

--- src/views/1-WordAdd.vue
+++ src/views/1-WordAdd.vue
@@ -1,10 +1,18 @@
 <template>
-    <div id="word-add">
+    <div id="word-add" class="fbox-v mh0">
+        <div id="page-desc">
+            <h2>Add Words</h2>
+            <p>You can use this page to simply add a word with its definition. If you added something by accident, you can remove words in the word lists page.</p>
+        </div>
+
         <div class="input-container">
             <input />
         </div>
         <div class="input-container">
             <input />
+        </div>
+        <div>
+            <el-button>Add</el-button>
         </div>
     </div>
 </template>
