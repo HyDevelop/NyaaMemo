a0cf9ec2a4f3fce410c96511d7a2934144b22c85
Hykilpikonna <me@hydev.org>
2021-10-07 00:51:11 -0400

    [S] WordAdd: Reimplement with hy-input

--- src/views/1-WordAdd.vue
+++ src/views/1-WordAdd.vue
@@ -14,17 +14,25 @@
                 <input v-model="definition"/>
             </div>
 
+            <hy-input placeholder="Word" v-model="word" v-if="mode === 2"></hy-input>
+            <hy-input placeholder="Definition" v-model="definition" v-if="mode === 2"></hy-input>
+            <hy-input placeholder="Alternative Names (colon separated)" v-model="definition" v-if="mode === 2"></hy-input>
+            <hy-input placeholder="" v-model="definition" v-if="mode === 2"></hy-input>
             <el-button id="btn-add" type="success" plain>Add</el-button>
         </div>
     </div>
 </template>
 
 <script>
-import {Vue} from "vue-class-component";
+import {Options, Vue} from "vue-class-component";
+import HyInput from "@/components/HyInput";
 
+@Options({components: {HyInput}})
 export default class WordAdd extends Vue
 {
-
+    mode = 2
+    word = ''
+    definition = ''
 }
 </script>
 
@@ -34,6 +42,9 @@ export default class WordAdd extends Vue
 #word-add
     width: $app-width
     margin: 0 auto
+
+    #content > *
+        margin-bottom: 20px
 
 .input-container
     margin-bottom: 20px
